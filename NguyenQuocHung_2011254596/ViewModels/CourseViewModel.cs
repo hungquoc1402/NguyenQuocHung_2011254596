e8ae9cf570c7692e1e555119b6c39ed941bde25b
hungquoc1402 <113426781+hungquoc1402@users.noreply.github.com>
2023-03-24 18:29:00 +0700

    Update CourseViewModel.cs
    Trang 35-51

--- NguyenQuocHung_2011254596/ViewModels/CourseViewModel.cs
+++ NguyenQuocHung_2011254596/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Build.Framework;
-using Microsoft.Build.Framework.XamlTypes;
+﻿
+
+using NguyenQuocHung_2011254596.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
